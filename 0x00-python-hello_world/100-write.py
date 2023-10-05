d71fbf6e7ebf1a265aac68696c2eda62c4ad099c
emmanuelist <emmanuel.paul75@yahoo.com>
2023-10-05 16:46:58 +0100

    Modify python script

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-msg = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(msg + "\n")
+msg = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(msg)
 sys.exit(1)

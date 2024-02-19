8b4bd35aa916b34ab65a11e4839f7772165f9d1a
emmanuelist <emmanuel.paul75@yahoo.com>
2024-02-19 17:00:17 +0100

    Implemented GitHub commits fetching script for efficiency and clarity

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Python script that fetches github"""
-import requests
 import sys
 
+import requests
 
 if __name__ == "__main__":
     # headers = {
     #     "Accept": "application/vnd.github.v3+json",
     #     "X-GitHub-Api-Version": "2022-11-28"
     # }
-    url = f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits'
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
     response = requests.get(url)
 
     user_data = response.json()
     for i in range(0, 10):
-        print(f'{user_data[i].get("sha")}: '
-              f'{user_data[i].get("commit").get("author").get("name")}')
+        print(
+            f'{user_data[i].get("sha")}: '
+            f'{user_data[i].get("commit").get("author").get("name")}'
+        )
